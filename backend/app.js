f45a27b80c48d3cf24530432bb553972e3960687
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2026-05-01 08:12:27 +0800

    fix: cloudinary config

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import cloudinary from '../config/cloudinary.js';
-
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './src/routes/auth.routes.js';
